b7273cdb084f8acd872e8bc1eb9450c0c626e85d
WebMucidi <fatihcelik1907@gmail.com>
2022-03-17 17:35:53 +0300

    Update script.js
    Create urunSecenek checkbox with value

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-let sutUrunleri=["peynir","sut","yoğurt"];
+let sutUrunleri=["peynir","40","sut","10","yoğurt","20"];
 let gidaUrunleri={"makarna":5,"bulgur":10,"pirinç":20};
 let temizlikUrunleri={"deterjan":50,"sabun":10,"çamaşır suyu":5};
 
-document.querySelectorAll("kategori")[0].addEventListener("change",urunleriGetir);
+
+//document.querySelectorAll("kategori")[0].addEventListener("change",urunleriGetir);
 
 function urunleriGetir(){
-    for(let i=0;i<sutUrunleri.length;i++)
+    for(let i=0;i<sutUrunleri.length;i=i+2)
     {
+
         let urunAciklama=document.createElement("label");
         let urunSecenek=document.createElement("input");
         document.getElementById("urunPanel").appendChild(urunAciklama);
         document.getElementById("urunPanel").appendChild(urunSecenek);
         urunSecenek.type="checkbox";
+        urunSecenek.setAttribute("name","urunler")
+        urunSecenek.value=sutUrunleri[i+1];
         urunAciklama.innerHTML=sutUrunleri[i];
-        urunSecenek.value=sutUrunleri[i];
+        urunAciklama.setAttribute("for","urunler")
+        
+
+
     }
 }
