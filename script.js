e8d8b71aff2a5f47176f3996753b2d79de452881
WebMucidi <fatihcelik1907@gmail.com>
2022-03-21 14:40:05 +0300

    pazartesi spete ekle

--- script.js
+++ script.js
@@ -1,24 +1,69 @@
 let sutUrunleri=["peynir","40","sut","10","yoğurt","20"];
-let gidaUrunleri={"makarna":5,"bulgur":10,"pirinç":20};
-let temizlikUrunleri={"deterjan":50,"sabun":10,"çamaşır suyu":5};
+let gidaUrunleri=["makarna","5","bulgur","10","pirinç","20"];
+let temizlikUrunleri=["deterjan","50","sabun","10","çamaşır suyu","5"];
+let i;
 
-//document.querySelectorAll("kategori")[0].addEventListener("change",urunleriGetir);
-
-console.log(sutUrunleri.length)
+for(i=0;i<document.getElementsByName("kategori").length;i++)
+{
+    document.getElementsByName("kategori")[i].addEventListener("change",urunleriGetir);
+}
 
 function urunleriGetir(){
-    //döngü
-    for(let i=0;i<sutUrunleri.length;i=i+2)
+    const silinecekler = document.getElementById("urunPanel");
+
+    while (silinecekler.hasChildNodes()) {
+      silinecekler.removeChild(silinecekler.firstChild);
+    }
+    
+    if(document.getElementById("kategoriSut").checked)
     {
+        
+    //döngü
+        for(i=0;i<sutUrunleri.length;i=i+2)
+        {
+            let urunAciklama=document.createElement("label");
+            let urunSecenek=document.createElement("input");
+            document.getElementById("urunPanel").appendChild(urunAciklama);
+            document.getElementById("urunPanel").appendChild(urunSecenek);
+            urunSecenek.type="checkbox";
+            urunSecenek.setAttribute("name","urunler")
+            urunSecenek.value=sutUrunleri[i+1];
+            urunAciklama.innerHTML=sutUrunleri[i];
+            urunAciklama.setAttribute("for","urunler");
+            urunAciklama.setAttribute("class","aciklama");
+        }
+    }
+    else if(document.getElementById("kategoriGida").checked)
+    {
+        for(i=0;i<gidaUrunleri.length;i=i+2)
+        {
         let urunAciklama=document.createElement("label");
         let urunSecenek=document.createElement("input");
         document.getElementById("urunPanel").appendChild(urunAciklama);
         document.getElementById("urunPanel").appendChild(urunSecenek);
         urunSecenek.type="checkbox";
         urunSecenek.setAttribute("name","urunler")
-        urunSecenek.value=sutUrunleri[i+1];
-        urunAciklama.innerHTML=sutUrunleri[i];
-        urunAciklama.setAttribute("for","urunler")
-
+        urunSecenek.value=gidaUrunleri[i+1];
+        urunAciklama.innerHTML=gidaUrunleri[i];
+        urunAciklama.setAttribute("for","urunler");
+        }
     }
+    else if(document.getElementById("kategoriTemizlik").checked)
+    {
+        for(i=0;i<temizlikUrunleri.length;i=i+2)
+        {
+        let urunAciklama=document.createElement("label");
+        let urunSecenek=document.createElement("input");
+        document.getElementById("urunPanel").appendChild(urunAciklama);
+        document.getElementById("urunPanel").appendChild(urunSecenek);
+        urunSecenek.type="checkbox";
+        urunSecenek.setAttribute("name","urunler")
+        urunSecenek.value=temizlikUrunleri[i+1];
+        urunAciklama.innerHTML=temizlikUrunleri[i];
+        urunAciklama.setAttribute("for","urunler");
+        }
+    }
+    
+
+
 }
