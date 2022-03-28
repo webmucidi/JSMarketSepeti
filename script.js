1a655fd869c10f5d56d1b9b84d6dc5762f30614b
WebMucidi <fatihcelik1907@gmail.com>
2022-03-28 15:11:42 +0300

    indirim kodu ve sepet hesaplama

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+//Ürün bilgileri dizi tipindeki değişkenlere alındı.
 let sutUrunleri=["peynir","40","sut","10","yoğurt","20"];
 let gidaUrunleri=["makarna","5","bulgur","10","pirinç","20"];
 let temizlikUrunleri=["deterjan","50","sabun","10","çamaşır suyu","5"];
+
+//Döngü için sayaç değişkeni
 let i;
+
+//Kategori seçimine göre doldurulacak ürünlerin input ve label tanımı için
 let urunAciklama,urunSecenek;
+
+//Sepete eklenecek ürünlerin ve ürünlere ait fiyatların ayrı ayrı taşınması için
 let eklenecekler=[];
+let fiyatlar=[];
+
+//Sepete ekleme,çıkarma ve boşaltma için sepet nesnesinin seçimi
 let listeSepet=document.getElementById("sepetMarket");
 
+//Ödenecek toplam tutarı başta sıfırlayarak tanımladık.
+let toplamTutar=0;
+
+//İndirim kodu için bir dizi veya değişken tanımlanabilir.
+const kod="PROMO10";
+
+//Kategori seçiminde ürünlerin güncellenmesi için olay yakalayıcı tanımlandı.
 for(i=0;i<document.getElementsByName("kategori").length;i++)
 {
     document.getElementsByName("kategori")[i].addEventListener("change",urunleriGetir);
 }   
 
+//Her kategori seçiminde gelecek ürünleri listeleyecek nesneler ve özellikleri tanımlandı
 function olustur(){
     urunAciklama=document.createElement("label");
     urunSecenek=document.createElement("input");
@@ -22,11 +40,15 @@ function olustur(){
     urunAciklama.setAttribute("class","urunler");
 }
 
+//Kategori seçimi yapıldıktan sonra ürün listesini(checkbox) doldurur.
 function urunleriGetir(){
+    //Ürün panelini her seferinde temizleyip yeniden doldurmak için 
     const silinecekler = document.getElementById("urunPanel");
     while (silinecekler.hasChildNodes()) {
       silinecekler.removeChild(silinecekler.firstChild);
     }
+
+    //Her kategoriye ait ürün içeriklerini dizi üzerinden aldık.
     if(document.getElementById("kategoriSut").checked)
     {
         for(i=0;i<sutUrunleri.length;i=i+2)
@@ -54,46 +76,89 @@ function urunleriGetir(){
         urunAciklama.innerHTML=temizlikUrunleri[i];
         }
     }
-
-    const listeUrunlerFiyat=document.getElementsByName("urunler");
-    const listeUrunlerAd=document.getElementsByClassName("urunler");
-    
-    document.addEventListener("change",()=>{
-        let urunFiyat=0;
-        eklenecekler=[];
-        for(i=0;i<listeUrunlerFiyat.length;i++){
-            if(listeUrunlerFiyat[i].checked){
-                urunFiyat+=Number(listeUrunlerFiyat[i].value);
-                eklenecekler.push(listeUrunlerAd[i].innerHTML);
-            }
-        }
-        console.log(urunFiyat);
-        console.log(eklenecekler);
-    }
-    );
 }
 
 function sepeteEkle(){
+    //Sepete eklenecek ürünleri alacağımız checkbox inputlar alındı.
+    const listeUrunlerFiyat=document.getElementsByName("urunler");
+    const listeUrunlerAd=document.getElementsByClassName("urunler");
+    
+    //Eklenecekler ve fiyatlar dizileri her ekleme sırasında sıfırlandı.
+        eklenecekler=[];
+        fiyatlar=[];
+
+        //Checkbox inputlarının hepsi gezilerek seçili olanlar dizilere eklendi.
+        for(i=0;i<listeUrunlerFiyat.length;i++){
+            if(listeUrunlerFiyat[i].checked){
+                toplamTutar+=Number(listeUrunlerFiyat[i].value);
+                eklenecekler.push(listeUrunlerAd[i].innerHTML);
+                fiyatlar.push(listeUrunlerFiyat[i].value);
+            }
+        }
+
+        //Eklenecekleri ve fiyatlarını alabildik mi diye bir bakalım?
+        console.log(eklenecekler);
+        console.log(fiyatlar);
+
+    //Eklenecek ürün adedi seçimi
     let adet=document.getElementById("urunAdet").value;
     console.log(adet);
 
+    //Eklenecek ürün adedi kadar aynı işlemi tekrar et
     for(i=0;i<adet;i++)
     {
+        let sepeteEklenecekUrun;
+        //Eklenecekler listesindeki herbir ürün için liste elemanlarını oluştur.
+        for(i=0;i<eklenecekler.length;i++){
+            sepeteEklenecekUrun=document.createElement("option");
+            listeSepet.options.add(sepeteEklenecekUrun);
+            sepeteEklenecekUrun.text=eklenecekler[i];
+            sepeteEklenecekUrun.value=fiyatlar[i];
+        }
+        /*
         eklenecekler.forEach(element => {
-            const urunSepet=document.createElement("option");
-            listeSepet.options.add(urunSepet);
-            urunSepet.text=element;
+            sepeteEklenecekUrun=document.createElement("option");
+            listeSepet.options.add(sepeteEklenecekUrun);
+            sepeteEklenecekUrun.text=element;
+            sepeteEklenecekUrun.value="Fiyat?";
+
         });
+        */
     }
+
+    //Ekleme işlemi yapılınca ara toplam tutarını yazdır.
+    document.getElementById("sepetTutar").innerHTML=toplamTutar+" TL";
 }
 
+//Sepetten seçili ürünü indeks değerine bakarak siler.
 function sepettenCikar(){
-    console.log(listeSepet.selectedIndex);
-    listeSepet.options.remove(listeSepet.selectedIndex);
+    //Seçili elemanın indeks sırasını ve valuesini alarak toplam tutardan düştük ve sildik.
+    let seciliIndeks=listeSepet.selectedIndex;
+    let silinecekUrununFiyati=listeSepet.options[seciliIndeks].value;
+    listeSepet.options.remove(seciliIndeks);
+    toplamTutar=toplamTutar-silinecekUrununFiyati;
+    document.getElementById("sepetTutar").innerHTML=toplamTutar+" TL";
 }
 
+//Tüm ürünleri sözel döngü fonksiyonuyla gezerek
 function sepetiBosalt(){
     document.querySelectorAll('#sepetMarket option').forEach(eleman => eleman.remove());
+    toplamTutar=0;
+    document.getElementById("sepetTutar").innerHTML=toplamTutar+" TL";
+
+}
+
+function koduEkle(){
+    let girilenKod=document.getElementById("txtIndirim").value;
+    if(girilenKod == kod)
+    {
+        toplamTutar=toplamTutar-10;
+        document.getElementById("sepetTutar").innerHTML=toplamTutar+" TL";
+        document.getElementById("sonuc").innerHTML="İndirim uygulandı.";
+    }
+    else{
+        document.getElementById("sonuc").innerHTML="Geçerli bir kod girmediniz!";
+    }
 }
 
 
